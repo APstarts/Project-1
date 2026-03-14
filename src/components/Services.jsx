4c244bb974087f19a2b764a6de97781cbf79bdcc
Anurag Pal <tanmaypal123@outlook.com>
2026-03-15 03:06:43 +0530

    added services section

---
+++ src/components/Services.jsx
@@ -0,0 +1,39 @@
+const Services = () => {
+  return (
+    <section id="services">
+      <h2>Services</h2>
+      <ul>
+        <li>
+          <p>Web Design</p>
+          <img
+            src="https://demoxml.com/wp/darkyn/wp-content/uploads/2025/03/branding.jpg"
+            alt="services-image-1"
+          ></img>
+        </li>
+        <li>
+          <p>Branding Design</p>{" "}
+          <img
+            src="https://demoxml.com/wp/darkyn/wp-content/uploads/2025/03/motion.jpg"
+            alt="services-image"
+          ></img>
+        </li>
+        <li>
+          <p>Motion Design</p>{" "}
+          <img
+            src="https://demoxml.com/wp/darkyn/wp-content/uploads/2025/03/webdesign.jpg"
+            alt="services-image"
+          ></img>
+        </li>
+        <li>
+          <p>Marketing</p>{" "}
+          <img
+            src="https://demoxml.com/wp/darkyn/wp-content/uploads/2025/03/marketing.jpg"
+            alt="services-image"
+          ></img>
+        </li>
+      </ul>
+    </section>
+  );
+};
+
+export default Services;
